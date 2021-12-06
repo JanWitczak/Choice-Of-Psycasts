abbd2beb4273cac16a7ab91836bd151f873dc846
Jan witczak <j.witczak@onet.pl>
2021-12-06 21:41:28 +0100

    Add project files.

---
+++ 1.2/Source/ChoiceOfPsycasts/ChoiceofPsycastsMod.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using UnityEngine;
+
+namespace RimWorld
+{
+    namespace ChoiceOfPsycasts
+    {
+        
+        public class ChoiceOfPsycastsProperties : CompProperties
+        {
+            public ChoiceOfPsycastsProperties()
+            {
+                compClass = typeof(ChoiceOfPsycastsComp);
+            }
+        }
+
+        public class ChoiceOfPsycastsComp : ThingComp
+        {
+            public List<int> CanLearnPsycast; 
+            public Pawn Parent
+            {
+                get { return (Pawn)this.parent; }
+            }
+
+            public override IEnumerable<Gizmo> CompGetGizmosExtra()
+            {
+                foreach (var i in CanLearnPsycast)
+                {
+                    yield return new LearnPsycasts(i, Parent);
+                }
+            }
+
+            public override void PostExposeData()
+            {
+                Scribe_Collections.Look(ref CanLearnPsycast, "CanLearnPsycast", LookMode.Value);
+                if (CanLearnPsycast == null) CanLearnPsycast = new List<int>();
+            }
+
+            public override void Initialize(CompProperties props)
+            {
+                Scribe_Collections.Look(ref CanLearnPsycast, "CanLearnPsycast", LookMode.Value);
+                if (CanLearnPsycast == null) CanLearnPsycast = new List<int>();
+            }
+        }
+
+        class LearnPsycasts : Command_Action
+        {
+            private int Level;
+            Pawn Parent;
+            public LearnPsycasts(int level, Pawn pawn)
+            {
+                action = Choice;
+                Level = level;
+                Parent = pawn;
+                defaultLabel = "Learn a Psycast";
+                defaultDesc = "Learn one psycast of level "+ Level.ToString();
+                icon = ContentFinder<Texture2D>.Get("Level" + level.ToString());
+            }
+
+            public void Choice()
+            {
+                List<FloatMenuOption> options = new List<FloatMenuOption>();
+                foreach (AbilityDef Psycast in AbilityLibrary.Psycasts[Level])
+                {
+                    FloatMenuOption option = new FloatMenuOption(Psycast.label, delegate
+                    {
+                        Parent.abilities.GainAbility(Psycast);
+                        Parent.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast.Remove(Level);
+                    });
+                    options.Add(option);
+                }
+                if (ChoiceOfPsycastsMod.Settings.PsycastOptions > 1)
+                {
+                    System.Random rnd = new System.Random(Level + Parent.GetHashCode());
+                    foreach (var i in Enumerable.Range(0, Math.Max(options.Count() - ChoiceOfPsycastsMod.Settings.PsycastOptions, 0)))
+                    {
+                        options.RemoveAt(rnd.Next(options.Count()));
+                    }
+                }
+                if (options.Count > 0)
+                {
+                    FloatMenu menu = new FloatMenu(options);
+                    Find.WindowStack.Add(menu);
+                }
+            }
+        }
+
+        [StaticConstructorOnStartup]
+        public static class AbilityLibrary
+        {
+            public static Dictionary<int, List<AbilityDef>> Psycasts = new Dictionary<int, List<AbilityDef>>();
+            static AbilityLibrary()
+            {
+                foreach(var i in Enumerable.Range(1,6))
+                {
+                    Psycasts.Add(i, new List<AbilityDef>());
+                }
+                List<AbilityDef> Abilities = DefDatabase<AbilityDef>.AllDefsListForReading;
+                foreach (AbilityDef Ability in Abilities)
+                {
+                    if (Ability.abilityClass == typeof(Psycast) && Ability.level > 0 && Ability.level < 7)
+                    {
+                        Psycasts[Ability.level].Add(Ability);
+                    }
+                }
+            }
+        }
+
+        public class ChoiceOfPsycastsSettings : ModSettings
+        {
+            public int PsycastOptions = 1;
+            public override void ExposeData()
+            {
+                Scribe_Values.Look(ref PsycastOptions, "PsycastOptions", defaultValue: 1); ;
+                base.ExposeData();
+            }
+        }
+
+        [StaticConstructorOnStartup]
+        public class ChoiceOfPsycastsMod : Verse.Mod
+        {
+            public static ChoiceOfPsycastsSettings Settings;
+            public ChoiceOfPsycastsMod(ModContentPack content) : base(content)
+            {
+                Settings = GetSettings<ChoiceOfPsycastsSettings>();
+            }
+
+            public override void DoSettingsWindowContents(Rect inRect)
+            {
+                Listing_Standard listingStandard = new Listing_Standard();
+                listingStandard.Begin(inRect);
+                if (Settings.PsycastOptions == 1)
+                    listingStandard.Label("Psycast Options: All");
+                else listingStandard.Label("Psycast Options: " + Settings.PsycastOptions.ToString());
+                Settings.PsycastOptions = (int)Math.Round(listingStandard.Slider(Settings.PsycastOptions, 1, 5), 0);
+                listingStandard.End();
+                base.DoSettingsWindowContents(inRect);
+            }
+           public override string SettingsCategory()
+            {
+                return "Choice Of Psycasts";
+            }
+        }
+    }
+}
---
+++ 1.3/Source/ChoiceOfPsycasts/ChoiceofPsycastsPatch.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace RimWorld
+{
+	namespace ChoiceOfPsycasts
+	{
+		public class HarmonyPatches : Verse.Mod
+		{
+			public HarmonyPatches(ModContentPack content) : base(content)
+			{
+				var harmony = new Harmony("Azuraal.ChoiceOfPsycasts");
+				var assembly = Assembly.GetExecutingAssembly();
+				harmony.PatchAll(assembly);
+			}
+
+		}
+
+		[HarmonyPatch(typeof(RimWorld.PawnUtility), "ChangePsylinkLevel")]
+		class ChangePsylinkLevelPatch
+		{
+			static void Postfix(ref Pawn pawn)
+			{
+				if (pawn.IsColonist)
+				{
+					if (pawn.GetComp<ChoiceOfPsycastsComp>() != null)
+					{
+						pawn.abilities.RemoveAbility(pawn.abilities.abilities[pawn.abilities.abilities.Count - 1].def);
+						if (pawn.GetPsylinkLevel() > 0 && pawn.GetPsylinkLevel() < 7)
+						{
+							if (pawn.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast == null) pawn.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast = new List<int>();
+							pawn.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast.Add(pawn.GetPsylinkLevel());
+						}
+						else Log.Error("Tried giving incorrect level Psycast");
+					}
+					else Log.Error("Pawn doesn't inherif after BasePawn and so doesn't have ChoiceOfPsycastsComp.");
+				}
+			}
+		}
+
+		[HarmonyPatch(typeof(CompUseEffect_InstallImplant), "DoEffect")]
+		class NeuroformerPatch
+		{
+			static void Postfix(ref Pawn user, CompUseEffect_InstallImplant __instance)
+			{
+				if (__instance.Props.hediffDef == DefDatabase<HediffDef>.GetNamed("PsychicAmplifier") && user.IsColonist)
+				{
+					if (user.GetComp<ChoiceOfPsycastsComp>() != null)
+					{
+						user.abilities.RemoveAbility(user.abilities.abilities[user.abilities.abilities.Count - 1].def);
+						if (user.GetPsylinkLevel() > 0 && user.GetPsylinkLevel() < 7)
+						{
+							if (user.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast == null) user.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast = new List<int>();
+							user.GetComp<ChoiceOfPsycastsComp>().CanLearnPsycast.Add(user.GetPsylinkLevel());
+						}
+						else Log.Error("Tried giving incorrect level Psycast");
+					}
+					else Log.Error("Pawn doesn't inherif after BasePawn and so doesn't have ChoiceOfPsycastsComp.");
+				}
+			}
+		}
+	}
+}
