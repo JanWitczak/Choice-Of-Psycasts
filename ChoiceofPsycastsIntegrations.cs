f934aaf0037539b44b5d4c82253cdd79c65e81c3
Jan witczak <j.witczak@onet.pl>
2022-05-26 09:55:22 +0200

    PsycstUpgradeFramework Integration

---
+++ ChoiceofPsycastsIntegrations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Verse;
+using System.Reflection;
+
+namespace ChoiceOfPsycasts
+{
+	[StaticConstructorOnStartup]
+	public class UpgradablePsycastsFrameworkIntegration
+	{
+		static public Type PUPExtension;
+		static public FieldInfo PUPField;
+		static UpgradablePsycastsFrameworkIntegration()
+		{
+			PUPExtension = Type.GetType("PsycastUpgradeFramework.PsycastExtension, PsycastUpgradeFramework", false);
+			if (PUPExtension != null) PUPField = PUPExtension.GetField("UpgradeOnly");
+		}
+	}
+}
